ecff046ffaecc95b23f64daee1eb8806638775e4
shubhrad1 <shubhramail.das@gmail.com>
2023-03-08 12:36:22 +0530

    Initial frontend dev

--- frontend/src/pages/blog/blog.jsx
+++ frontend/src/pages/blog/blog.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Blog from "./blog.module.css";
 import axios from 'axios';
-import { Links } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const BlogPost = () => {
     const [blogs,setBlogs]=useState([]);
@@ -12,7 +12,7 @@ const BlogPost = () => {
             try {
                 const res=await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/featured`);
                 setFeaturedBlog(res.data[0]);
-                console.log(process.env.REACT_APP_API_URL);
+                
             }
             catch(err)
             {
@@ -45,7 +45,35 @@ const BlogPost = () => {
     };
 
     const getBlogs = () => {
+        let list=[];
+        let result=[];
 
+        blogs.map(blogPost => {
+            
+            return list.push(
+                <div className={Blog.blogtile}>
+                    
+                    <div>Title{blogPost.title}</div>
+                    <div>Category:{blogPost.category}</div>
+                </div>
+            );
+        });
+
+        for(let i=0;i<list.length;i+=2){
+        result.push(
+            <div key={i} className={Blog.blogArea}>
+                <div>
+                    {list[i]}
+                </div>
+                <div>
+                    {list[i+1]?list[i+1]:null}
+                </div>
+
+            </div>
+        );
+        }
+
+        return result;
     };
 
     return(
@@ -53,15 +81,29 @@ const BlogPost = () => {
             <div className={Blog.header}>
                 Blog<font color="#0FFF50"><b>_</b></font>Net
             </div>
+            <div className={Blog.navbar}>
+                
+                    <div className={Blog.navbar_box}><Link>Link1</Link></div>
+                    <div className={Blog.navbar_box}><Link>Link2</Link></div>
+                    <div className={Blog.navbar_box}><Link>Link3</Link></div>
+                    <div className={Blog.navbar_box}><Link>Link4</Link></div>
+                    <div className={Blog.navbar_box}><Link>Link5</Link></div>
+                    <div className={Blog.navbar_box}><Link>Link6</Link></div>
+                
+            </div>
             <div className={Blog.body}>
-                Body
+                <div className={Blog.blog_card}>
+                    <div className={Blog.blog_title}>{featuredBlog.title}</div>
+                    <div className={Blog.blog_content}>{featuredBlog.excerpt}</div>
+                    <div className={Blog.continue_read}><Link to={`/blog/${featuredBlog.slug}`}>Continue Reading.....</Link></div>
+                </div>
+                {/* <div className={Blog.blogArea}> */}
+                    {getBlogs()}
+                {/* </div> */}
             </div>
-
-            <div className={Blog.blog_card}>
-                <div className={Blog.blog_title}>{featuredBlog.title}</div>
-                <div className={Blog.blog_content}>con</div>
-            </div>
+            
         </div>
+            
     );
 };
 export default BlogPost;
