e1cca0099d8881eb9a8cb5c3b72f420f783f185f
shubhrad1 <shubhramail.das@gmail.com>
2023-03-10 00:18:34 +0530

    Deplo Tesr

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
