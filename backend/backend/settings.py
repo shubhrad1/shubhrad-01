d5b608a415d1d08e37863ea4de337853e5bfd0d2
shubhrad1 <shubhramail.das@gmail.com>
2023-05-14 21:07:37 +0530

    Production Test-1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['20.120.38.207']
 
 
 # Application definition
