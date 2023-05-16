f2967c909515d423103918964285fea4fc1c2c69
shubhrad1 <shubhramail.das@gmail.com>
2023-05-17 04:13:29 +0530

    Static url changes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
@@ -10,5 +11,5 @@ urlpatterns = [
     path('api/blog/',include('blog.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
